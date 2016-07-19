5057c9b8ca77c8898d0c7b8f9d1803a82b9a408e
nurulc <nurulc@yahoo.com>
2016-07-19 06:09:13 -0400

    testing code

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,26 +4,30 @@ import React from 'react'
 import {render} from 'react-dom'
 
 import MaskedInput from '../../src'
+import RX from 'incr-regex-package'
 
 const PATTERNS = [
   '1111 1111',
   '111 111',
   '11 11',
   '1 1'
-]
+];
+
+window.RX = RX;
 
 const App = React.createClass({
   getInitialState() {
     return {
-      card: '',
-      expiry: '',
+      card:  '',
+      email: '',
+      tester: '',
       ccv: '',
       plate: '',
       escaped: '',
       leading: '',
       custom: '',
       changing: '',
-      pattern: '1111 1111'
+      pattern: '1111 1111' 
     }
   },
 
@@ -38,14 +42,64 @@ const App = React.createClass({
   },
 
   render() {
-    return <div className="App">
+    var rx = "1111112";  ///123-...456/;
+    var rx1 = new RegExp("\\+\\(\\d{3}\\)-\\d{3}-\\d{4}|#\\d{3}\.\\d{3}X?YZ| ?\\d{3}---\\d{4}\\.");
+    var rz2 = /aa[a-zA-Z]+@@\d+!!/;
+    var email = /[a-zA-Z_0-9][a-zA-Z_.0-9-]*@([a-zA-Z_0-9][a-zA-Z_.0-9-]*)+/
+    //console.log("here I am",rx, RX.RXInputMask);
+    return (
+      <div className="App">
+      <h1>
+        <code>&lt;<a href="https://github.com/insin/react-maskedinput">MaskedInput</a>/&gt;</code>
+      </h1>
+      <p></p>
+      <p className="lead">A React component which creates a masked <code>&lt;input/&gt;</code></p>
+      <div className="form-field" style={ {verticalAlign: 'top'} } >
+        <label htmlFor="card">Card Number xxx:</label>
+        <MaskedInput mask={rx1} name="card" id="card" size="20" value={this.state.card} onChange={this._onChange}/>
+      </div>
+      <div className="form-field">
+        <label htmlFor="email">Email:</label>
+        <MaskedInput mask={email} name="mail" id="mail" size="40" value={this.state.email} onChange={this._onChange}/>
+      </div>
+      <div className="form-field">
+        <label htmlFor="tester">Tester:</label>
+        <MaskedInput mask={rz2} name="mail" id="tester" size="40" value={this.state.tester} onChange={this._onChange}/>
+      </div>
+   
+      </div>  
+      )
+  }
+})
+
+const CustomInput = React.createClass({
+  render() {
+    return <MaskedInput
+      mask="1111-WW-11"
+      placeholder="1234-WW-12"
+      placeholderChar=" "
+      size="11"
+      {...this.props}
+      formatCharacters={{
+        'W': {
+          validate(char) { return /\w/.test(char) },
+          transform(char) { return char.toUpperCase() }
+        }
+      }
+    }/>
+  }
+})
+
+render(<App/>, document.getElementById('demo'))
+/*
+<div className="App">
       <h1>
         <code>&lt;<a href="https://github.com/insin/react-maskedinput">MaskedInput</a>/&gt;</code>
       </h1>
       <p className="lead">A React component which creates a masked <code>&lt;input/&gt;</code></p>
       <div className="form-field">
-        <label htmlFor="card">Card Number:</label>
-        <MaskedInput mask="1111 1111 1111 1111" name="card" id="card" size="20" value={this.state.card} onChange={this._onChange}/>
+        <label htmlFor="card">Card Number xxx:</label>
+        <MaskedInput mask={rx1} name="card" id="card" size="20" value={this.state.card} onChange={this._onChange}/>
       </div>
       <p>You can even externally update the card state like a standard input element:</p>
       <div className="form-field">
@@ -96,25 +150,4 @@ const App = React.createClass({
       <hr/>
       <footer><a href="https://github.com/insin/react-maskedinput">Source on GitHub</a></footer>
     </div>
-  }
-})
-
-const CustomInput = React.createClass({
-  render() {
-    return <MaskedInput
-      mask="1111-WW-11"
-      placeholder="1234-WW-12"
-      placeholderChar=" "
-      size="11"
-      {...this.props}
-      formatCharacters={{
-        'W': {
-          validate(char) { return /\w/.test(char) },
-          transform(char) { return char.toUpperCase() }
-        }
-      }
-    }/>
-  }
-})
-
-render(<App/>, document.getElementById('demo'))
+*/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 var React = require('react')
 var {getSelection, setSelection} = require('react/lib/ReactInputSelection')
+var RX = require('incr-regex-package').default;
 
 var InputMask = require('inputmask-core')
+
 
 var KEYCODE_Z = 90
 var KEYCODE_Y = 89
@@ -14,9 +16,23 @@ function isRedo(e) {
   return (e.ctrlKey || e.metaKey) && e.keyCode === (e.shiftKey ? KEYCODE_Z : KEYCODE_Y)
 }
 
+function supportArrowNavigation(mask) {
+   return (RX.contract.isFunc(mask.arrowAction));  
+}
+
+function asStr(anObj) {
+  return JSON.stringify(anObj);
+}
+
+function eqSel(sel1,sel2) {
+  if( sel1 === sel2 ) return true;
+  if( sel1 === undefined || sel2 === undefined) return false;
+  return sel1.start === sel2.start && sel1.end === sel2.end;
+}
+
 var MaskedInput = React.createClass({
   propTypes: {
-    mask: React.PropTypes.string.isRequired,
+    mask: React.PropTypes.object.isRequired,
 
     formatCharacters: React.PropTypes.object,
     placeholderChar: React.PropTypes.string
@@ -28,6 +44,10 @@ var MaskedInput = React.createClass({
     }
   },
 
+  getInitialState() {
+     return { focus: false};
+  },
+
   componentWillMount() {
     var options = {
       pattern: this.props.mask,
@@ -37,10 +57,14 @@ var MaskedInput = React.createClass({
     if (this.props.placeholderChar) {
       options.placeholderChar = this.props.placeholderChar
     }
-    this.mask = new InputMask(options)
+    //var rx = new RX.RXInputMask(options);
+    
+    this.mask = (typeof options.mask === "string") ?  new InputMask(options) : new RX.RXInputMask(options);
+    console.log("MASK HAS BEEN SET", options, this.mask);
   },
 
   componentWillReceiveProps(nextProps) {
+    console.log("HERE I AAM - componentWillReceiveProps", nextProps);
     if (this.props.value !== nextProps.value) {
       this.mask.setValue(nextProps.value)
     }
@@ -50,15 +74,27 @@ var MaskedInput = React.createClass({
   },
 
   _updateMaskSelection() {
-    this.mask.selection = getSelection(this.input)
+     console.log("HERE I AAM - _updateMaskSelection", getSelection(this.input));
+    this.mask.selection = getSelection(this.input);
+     console.log("HERE I AAM - _updateMaskSelection done", this.mask.selection);
   },
 
   _updateInputSelection() {
-    setSelection(this.input, this.mask.selection)
+    console.log("HERE I AAM - _updateInputSelection", this.mask.selection, this.mask.getValue());
+    if( !eqSel(getSelection(this.input),this.mask.selection)) setSelection(this.input, this.mask.selection);
+    console.log("HERE I AAM - _updateInputSelection DONE", getSelection(this.input));
+  },
+
+  _onFocus(e) {
+
+  },
+
+  _onBlurr() {
+
   },
 
   _onChange(e) {
-    // console.log('onChange', JSON.stringify(getSelection(this.input)), e.target.value)
+     console.log('onChange', asStr(getSelection(this.input)), e.target.value)
 
     var maskValue = this.mask.getValue()
     if (e.target.value !== maskValue) {
@@ -67,9 +103,10 @@ var MaskedInput = React.createClass({
         var sizeDiff = maskValue.length - e.target.value.length
         this._updateMaskSelection()
         this.mask.selection.end = this.mask.selection.start + sizeDiff
-        this.mask.backspace()
+        this.mask.backspace();
+        console.log("Fix maskValue", maskValue, "diff:", sizeDiff, "target value: ", e.target.value);
       }
-      var value = this._getDisplayValue()
+      var value = this._getDisplayValue();
       e.target.value = value
       if (value) {
         this._updateInputSelection()
@@ -81,13 +118,14 @@ var MaskedInput = React.createClass({
   },
 
   _onKeyDown(e) {
-    // console.log('onKeyDown', JSON.stringify(getSelection(this.input)), e.key, e.target.value)
+     console.log('onKeyDown',this.mask, asStr(getSelection(this.input))+"/"+asStr(this.mask.selection), e.key, e.keyCode, e.target.value)
 
     if (isUndo(e)) {
       e.preventDefault()
       if (this.mask.undo()) {
-        e.target.value = this._getDisplayValue()
-        this._updateInputSelection()
+        e.target.value = this._getDisplayValue();
+        console.log("undo:getDisplayValue", this._getDisplayValue());
+        this._updateInputSelection();
         this.props.onChange(e)
       }
       return
@@ -95,19 +133,53 @@ var MaskedInput = React.createClass({
     else if (isRedo(e)) {
       e.preventDefault()
       if (this.mask.redo()) {
-        e.target.value = this._getDisplayValue()
+        e.target.value = this._getDisplayValue();
+        console.log("redo:getDisplayValue", this._getDisplayValue());
         this._updateInputSelection()
         this.props.onChange(e)
       }
       return
     }
 
-    if (e.key === 'Backspace') {
+    if( e.key === 'ArrowLeft' || e.key == 'ArrowRight') {
+      // Check if mask supports arrow support
+      let sel = getSelection(this.input);
+      this.mask.selection = sel;
+      if( sel.start === sel.end && this.mask.left !== undefined) {
+        e.preventDefault();
+        if( e.key === 'ArrowLeft' ) this.mask.left(sel);
+        else this.mask.right(sel);
+        this._updateInputSelection();
+    }
+/*
+      if(this.mask.selection.start == this.mask.selection.end){
+        if(e.key === "ArrowLeft") {
+          let ix = this.mask.pattern.getFirstEditableBefore(this.mask.selection.start-1);
+          if(ix === undefined || ix !== this.mask.selection.start-1) {
+            e.preventDefault();
+            this.mask.selection.start=this.mask.selection.end = ix;
+            this._updateInputSelection();
+          } 
+        }
+        else if(e.key === "ArrowRight") {
+          let ix = this.mask.pattern.getFirstEditableAfter(this.mask.selection.start+1);
+          if(ix === undefined || ix !== this.mask.selection.start+1) {
+            e.preventDefault();
+            this.mask.selection.start=this.mask.selection.end = ix;
+            this._updateInputSelection();
+          } 
+        } 
+      } 
+*/
+      console.log("Arrow Action support:", supportArrowNavigation(this.mask), " value:",this._getDisplayValue(), " selection: ", asStr(getSelection(this.input)), asStr(this.mask.selection));
+    }
+    else if (e.key === 'Backspace') {
       e.preventDefault()
       this._updateMaskSelection()
       if (this.mask.backspace()) {
         var value = this._getDisplayValue()
-        e.target.value = value
+        e.target.value = value;
+        console.log("backspace:getDisplayValue", this._getDisplayValue());
         if (value) {
           this._updateInputSelection()
         }
@@ -117,29 +189,32 @@ var MaskedInput = React.createClass({
   },
 
   _onKeyPress(e) {
-    // console.log('onKeyPress', JSON.stringify(getSelection(this.input)), e.key, e.target.value)
+     console.log('onKeyPress', asStr(getSelection(this.input)),asStr(this.mask.selection), e.key, e.target.value)
 
     // Ignore modified key presses
     // Ignore enter key to allow form submission
     if (e.metaKey || e.altKey || e.ctrlKey || e.key === 'Enter') { return }
-
-    e.preventDefault()
-    this._updateMaskSelection()
+    let selX = getSelection(this.input);
+    let oldMaskX = this.mask.getSelection();
+    e.preventDefault();
+    this._updateMaskSelection();
     if (this.mask.input(e.key)) {
-      e.target.value = this.mask.getValue()
-      this._updateInputSelection()
-      this.props.onChange(e)
+      e.target.value = this.mask.getValue();
+      console.log("keyPress:getDisplayValue", this._getDisplayValue(),  " selection: ", asStr(selX)+"/"+asStr(this.mask.selection)+"<"+asStr(oldMaskX));
+      this._updateInputSelection();
+      this.props.onChange(e);
     }
   },
 
   _onPaste(e) {
-    // console.log('onPaste', JSON.stringify(getSelection(this.input)), e.clipboardData.getData('Text'), e.target.value)
+     console.log('onPaste', asStr(getSelection(this.input)), e.clipboardData.getData('Text'), e.target.value)
 
     e.preventDefault()
     this._updateMaskSelection()
     // getData value needed for IE also works in FF & Chrome
     if (this.mask.paste(e.clipboardData.getData('Text'))) {
-      e.target.value = this.mask.getValue()
+      e.target.value = this.mask.getValue();
+      console.log("undo:getDisplayValue", this._getDisplayValue());
       // Timeout needed for IE
       setTimeout(this._updateInputSelection, 0)
       this.props.onChange(e)
@@ -153,7 +228,8 @@ var MaskedInput = React.createClass({
 
   render() {
     var {mask, formatCharacters, size, placeholder, ...props} = this.props
-    var patternLength = this.mask.pattern.length
+    var patternLength = this.mask.pattern.length;
+    console.log("about to render");
     return <input {...props}
       ref={r => this.input = r }
       maxLength={patternLength}
@@ -164,6 +240,7 @@ var MaskedInput = React.createClass({
       placeholder={placeholder || this.mask.emptyValue}
       size={size || patternLength}
       value={this._getDisplayValue()}
+      style={{padding: "3px 0px 3px 0px"}}
     />
   }
 })
